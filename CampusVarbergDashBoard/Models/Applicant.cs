7916a8cb8d09a52c8ae089861d7af936f7e6e393
Christian Rapp <christian.rapp@edu.varberg.se>
2024-10-01 15:10:29 +0200

    Gjort lite småfix

--- CampusVarbergDashBoard/Models/Applicant.cs
+++ CampusVarbergDashBoard/Models/Applicant.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
 using System.Runtime.CompilerServices;
 
 namespace CampusVarbergDashBoard.Models
@@ -6,21 +7,21 @@ namespace CampusVarbergDashBoard.Models
     public class Applicant
     {
         public string Utbildning { get; set; }
-        public DateTime Fodelsedatum { get; set; }
-        public string Kon { get; set; }
+        public DateTime Födelsedatum { get; }
+        public string Kön { get; set; }
         public string Postnummer { get; set; }
         public string Ort { get; set; }
         public string Land { get; set; }
-        public DateTime Registrerad { get; set; }
-        public DateTime Inlamnad { get; set; }
-        public bool Behorig { get; set; }
+        public DateTime Registrerad { get; }
+        public DateTime Inlämnad { get; }
+        public string Behörig { get; set; }
         public bool SenAnmalan { get; set; }
         public string Status { get; set; }
         public bool ErbjudenPlats { get; set; }
         public string Urval { get; set; }
         public double Longitud { get; set; }
         public double Latitude { get; set; }
-        public int Alder { get; set; }
+        public int Ålder { get; set; }
 
     }
 }
