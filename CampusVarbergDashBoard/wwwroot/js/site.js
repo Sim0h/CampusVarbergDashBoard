f62249ac0b51f75d6ea992280316ac1778797d81
Rasmus Glasell Johansson <ragl025@gmail.com>
2024-10-15 10:06:44 +0200

    navbar fixed to top on scroll

--- CampusVarbergDashBoard/wwwroot/js/site.js
+++ CampusVarbergDashBoard/wwwroot/js/site.js
@@ -1,26 +1,18 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
 
-// Write your JavaScript code.
+const header = document.querySelector('header');
+const headerHeight = header.offsetHeight;
+const hiddenNavbar = document.getElementById('hiddenNavbar')
 
-let darkmode = localStorage.getItem('darkmode')
-const darkmodeToggle = document.getElementById('darkmode-toggle')
 
-const enableDarkmode = () => {
-    document.body.classList.add('darkmode')
-    localStorage.setItem('darkmode', 'active')
-    darkmodeToggle.checked = true
+function onScroll() {
+    if (window.pageYOffset > headerHeight) {
+        hiddenNavbar.style.display = 'block';
+    }
+    else {
+        hiddenNavbar.style.display = 'none';
+    }
 }
 
-const disableDarkmode = () => {
-    document.body.classList.remove('darkmode')
-    localStorage.setItem('darkmode', null)
-    darkmodeToggle.checked = false
-}
+window.addEventListener('scroll', onScroll);
 
-if (darkmode === "active") enableDarkmode()
-
-darkmodeToggle.addEventListener("click", () => {
-    darkmode = localStorage.getItem('darkmode')
-    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
-})
